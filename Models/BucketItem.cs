a281185617c1f1f22d8b6f209f92e2d6edce342f
G300000 <Presheun5@gmail.com>
2026-01-21 08:27:21 +0800

    Update BucketItem.cs

--- Models/BucketItem.cs
+++ Models/BucketItem.cs
@@ -1,7 +1,10 @@
-﻿namespace Bucket_list_mvc.Models
+using System.ComponentModel.DataAnnotations;
+
+namespace Bucket_list_mvc.Models
 {
     public class BucketItem
     {
+        [Key]
         public int Id { get; set; }
         public string ActivityName { get; set; } = string.Empty;
         public bool IsCompleted { get; set; } = false;
